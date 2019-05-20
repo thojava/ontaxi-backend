cab15f6deae40fd3bb454e15e3c501b1b8576dac
Tho Le Duc <thold88@gmail.com>
2019-05-20 11:34:56 +0700

    Update the menu a bit.

--- taxihub/src/main/java/vn/ontaxi/hub/component/DriversMapComponent.java
+++ taxihub/src/main/java/vn/ontaxi/hub/component/DriversMapComponent.java
@@ -2,9 +2,12 @@ package vn.ontaxi.hub.component;
 
 import com.google.gson.Gson;
 import org.springframework.stereotype.Component;
+import vn.ontaxi.common.jpa.entity.Driver;
 import vn.ontaxi.common.model.LocationWithDriver;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -13,6 +16,10 @@ public class DriversMapComponent {
         Map<String, LocationWithDriver> onlineDriverMap = new HashMap<>();
 
         return new Gson().toJson(onlineDriverMap);
+    }
+
+    public List<Driver> getOnlineDriversLocation(boolean bol) {
+        return new ArrayList<>();
     }
 
 
