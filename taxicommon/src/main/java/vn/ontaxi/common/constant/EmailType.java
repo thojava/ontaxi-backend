1e5bc0c4ff2fec88abb0ef62bc2cc36f613f9520
Tho Le Duc <thold88@gmail.com>
2019-08-11 18:52:42 +0700

    Add new email type

--- taxicommon/src/main/java/vn/ontaxi/common/constant/EmailType.java
+++ taxicommon/src/main/java/vn/ontaxi/common/constant/EmailType.java
@@ -2,10 +2,13 @@ package vn.ontaxi.common.constant;
 
 public enum EmailType {
     MARKETING("Marketing"),
-    SET_PASSWORD("Khách hàng nhập password cho account mới"),
-    ACCOUNT_ACTIVATED_NOTIFICATION("Thông báo cho khách hàng sau khi tài khoản được kích hoạt"),
-    RESET_PASSWORD("Khách hàng reset password cho account"),
+
+    REQUEST_RESET_PASSWORD("Khách hàng reset password cho account"),
+    RESET_PASSWORD_SUCCESSFUL("Khách hàng reset password thành công"),
+
     ACTIVE_ACCOUNT("Khách hàng kích hoạt tài khoản"),
+    ACCOUNT_ACTIVATED_SUCCESSFUL("khách hàng kích hoạt tài khoản thành công"),
+
     BOOKING_SUCCESS_CONFIRMATION("Thông báo đặt xe thành công");
 
     private String description;
