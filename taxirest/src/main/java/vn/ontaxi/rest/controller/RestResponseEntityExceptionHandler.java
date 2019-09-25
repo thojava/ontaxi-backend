dfd4159618f9a212db30b94f6037b9f70ae6c1bf
Hopnv <hopnv.1611@gmail.com>
2019-09-25 16:32:25 +0700

    add log when error 400

--- taxirest/src/main/java/vn/ontaxi/rest/controller/RestResponseEntityExceptionHandler.java
+++ taxirest/src/main/java/vn/ontaxi/rest/controller/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package vn.ontaxi.rest.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -12,13 +13,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(RestDriverController.class);
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public void handle(HttpMessageNotReadableException e) {
         //logger.warn("Returning HTTP 400 Bad Request", e);
         //System.out.println();
-        e.printStackTrace();
-        System.out.println(e.getMessage());
+        logger.error(e.getMessage());
+        logger.error("error", e);
     }
 }
