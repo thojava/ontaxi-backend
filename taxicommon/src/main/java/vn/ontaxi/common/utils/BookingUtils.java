4eb49c14632474c56f5c455208e3c6ca7f328119
Tho Le Duc <thold88@gmail.com>
2019-12-12 16:56:38 +0700

    Enable sound for FCM notification

--- taxicommon/src/main/java/vn/ontaxi/common/utils/BookingUtils.java
+++ taxicommon/src/main/java/vn/ontaxi/common/utils/BookingUtils.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
