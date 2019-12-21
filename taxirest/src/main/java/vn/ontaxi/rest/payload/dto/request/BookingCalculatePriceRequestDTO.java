ac305f99657255a35940afe640d3fb2deb4f5f52
Tho Le Duc <thold88@gmail.com>
2019-12-21 18:30:43 +0700

    Add option driver_will_wait for rounded trip

--- taxirest/src/main/java/vn/ontaxi/rest/payload/dto/request/BookingCalculatePriceRequestDTO.java
+++ taxirest/src/main/java/vn/ontaxi/rest/payload/dto/request/BookingCalculatePriceRequestDTO.java
@@ -1,6 +1,5 @@
 package vn.ontaxi.rest.payload.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import vn.ontaxi.common.constant.CarTypes;
 
@@ -13,6 +12,7 @@ public class BookingCalculatePriceRequestDTO {
     private String to_location;
     private Date departureTime;
     private String is_round_trip;
+    private boolean driver_will_wait;
     private Date returnDepartureTime;
     @Enumerated(EnumType.STRING)
 
@@ -69,5 +69,14 @@ public class BookingCalculatePriceRequestDTO {
 
     public void setReturnDepartureTime(Date returnDepartureTime) {
         this.returnDepartureTime = returnDepartureTime;
+    }
+
+    @ApiModelProperty(example = "true", value="Driver will wait customer to return or not")
+    public boolean isDriver_will_wait() {
+        return driver_will_wait;
+    }
+
+    public void setDriver_will_wait(boolean driver_will_wait) {
+        this.driver_will_wait = driver_will_wait;
     }
 }
