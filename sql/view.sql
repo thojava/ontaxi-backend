7550ffd0cb517f5f1df6104ab5a901cd383191a4
Tho Le Duc <thold88@gmail.com>
2019-12-24 10:54:43 +0700

    Update SQL view for DriverTransaction

--- sql/view.sql
+++ sql/view.sql
@@ -1,5 +1,5 @@
 drop view driver_transaction;
 CREATE VIEW driver_transaction AS
-  SELECT version, concat('P', id) as id, driver, amount, payment_type, reason, created_by, created_datetime, last_updated_datetime FROM driver_payment
+  SELECT version, concat('P', id) as id, driver, amount, payment_type, reason, created_by, modified_by, created_datetime, last_updated_datetime FROM driver_payment
   UNION
-  select version, concat('B', id) as id, accepted_by, actual_total_fee, 'UC', concat('Phí Order Số', id) as reason, created_by, created_datetime, last_updated_datetime from booking where status = 'C';
+  select version, concat('B', id) as id, accepted_by, actual_total_fee, 'UC', concat('Phí Order Số', id) as reason, created_by, modified_by, created_datetime, last_updated_datetime from booking where status = 'C';
