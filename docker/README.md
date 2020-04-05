a1b8a6eb717c4d5d1327360ee33a5b9c6d266e9d
Tho Le Duc <thold88@gmail.com>
2020-04-05 14:57:41 +0700

    Enhance mysql-s3-back dockerfile to use passed environment variables

---
+++ docker/README.md
@@ -0,0 +1,2 @@
+docker build . -t thold88/mysql-s3-backup:1.0.0
+docker push thold88/mysql-s3-backup:1.0.0
--- docker/mysqlbackup-tos3.sh
+++ docker/mysqlbackup-tos3.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-mysqldump -h mysql -u root -padm1n ontaxi > ontaxi.sql
-export AWS_ACCESS_KEY_ID=AKIAQFNFYMQHBMSXWIWK
-export AWS_SECRET_ACCESS_KEY=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
-s3cmd put ontaxi.sql* s3://taxi-mysql-backup/ontaxi.sql
+source /root/project_env.sh
+mysqldump -h mysql -u "$MYSQL_USER" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" > "${MYSQL_DATABASE}".sql
+s3cmd put "${MYSQL_DATABASE}".sql s3://taxi-mysql-backup/"${MYSQL_DATABASE}".sql
 
 logger "database backup, upload to s3 and cleanup of old backups completed"
